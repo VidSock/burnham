0d30e433a3306375c502259e95b4bd14edcb0923
twilightscapes <ihatespam@gmail.com>
2020-07-31 12:05:53 -0500

    Update Install.js

--- src/components/Install.js
+++ src/components/Install.js
@@ -43,7 +43,7 @@ const Install = () => (
 
 
 <CookieConsent
-	debug={false}
+	debug={true}
 	location="none"
 	style={{ color:'inherit', width:'auto', background:'inherit',  alignItems:'inherit',  textAlign:'left', justifyContent:'center', margin:'0 auto', top:'60px', maxWidth:'600px', position:'', border:'0px solid red', }}
     className=""
